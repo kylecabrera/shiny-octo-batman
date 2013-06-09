75b488e8ed5af6118cb6c6ab3df4cce1a25ad9c2
foxtrot <foxtrot@FoxtrotMk1.(none)>
2013-06-09 13:06:52 -0500

    Created index page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moms::Application.config.secret_token = 'b38eee1e8806ea3a16bd64102cdabc215343b89e8ec580df817885e5b39ccdaa104e31f524a74c644e6e2ab7f6ce979e7e66a9e21ad7b9f50e983ace6da988ab'
